e55dc555b506f5f6c91af9f54dbb747264ada82e
Arne Wouters <arne@wouters.co>
2021-05-04 15:19:57 +0200

    Add fold_call_agent that does 50/50 call-fold

---
+++ FCPA/fold_call_agent/fcpa_agent.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+fcpa_agent.py
+
+Extend this class to provide an agent that can participate in a tournament.
+
+Created by Pieter Robberechts, Wannes Meert.
+Copyright (c) 2021 KU Leuven. All rights reserved.
+"""
+
+import sys
+import argparse
+import logging
+import numpy as np
+import pyspiel
+from open_spiel.python.algorithms import evaluate_bots
+
+
+logger = logging.getLogger('be.kuleuven.cs.dtai.fcpa')
+
+
+def get_agent_for_tournament(player_id, seed=None):
+    """Change this function to initialize your agent.
+    This function is called by the tournament code at the beginning of the
+    tournament.
+
+    :param player_id: The integer id of the player for this bot, e.g. `0` if
+        acting as the first player.
+    """
+    my_player = Agent(player_id, seed)
+    return my_player
+
+
+class Agent(pyspiel.Bot):
+    """Agent template"""
+
+    def __init__(self, player_id, seed):
+        """Initialize an agent to play FCPA poker.
+
+        Note: This agent should make use of a pre-trained policy to enter
+        the tournament. Initializing the agent should thus take no more than
+        a few seconds.
+        """
+        pyspiel.Bot.__init__(self)
+        self.player_id = player_id
+        if seed is None:
+            self.rng = np.random.RandomState()
+        else:
+            self.rng = np.random.RandomState(seed)
+
+    def restart_at(self, state):
+        """Starting a new game in the given state.
+
+        :param state: The initial state of the game.
+        """
+        pass
+
+    def inform_action(self, state, player_id, action):
+        """Let the bot know of the other agent's actions.
+
+        :param state: The current state of the game.
+        :param player_id: The ID of the player that executed an action.
+        :param action: The action which the player executed.
+        """
+        pass
+
+    def step(self, state):
+        """Returns the selected action in the given state.
+
+        :param state: The current state of the game.
+        :returns: The selected action from the legal actions, or
+            `pyspiel.INVALID_ACTION` if there are no legal actions available.
+        """
+        legal_actions = state.legal_actions()
+        action = self.rng.choice(legal_actions[:2], p=[0.5, 0.5])
+
+        return action
+
+
+def test_api_calls():
+    """This method calls a number of API calls that are required for the
+    tournament. It should not trigger any Exceptions.
+    """
+    fcpa_game_string = pyspiel.hunl_game_string("fcpa")
+    game = pyspiel.load_game(fcpa_game_string)
+    bots = [get_agent_for_tournament(player_id) for player_id in [0,1]]
+    returns = evaluate_bots.evaluate_bots(game.new_initial_state(), bots, np.random)
+    assert len(returns) == 2
+    assert isinstance(returns[0], float)
+    assert isinstance(returns[1], float)
+    print("SUCCESS!")
+
+
+def main(argv=None):
+    test_api_calls()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
